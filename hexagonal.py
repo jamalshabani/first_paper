80afcc139b79bd80364232052a4f42cb50a64275
jamalshabani <jjuma1992@gmail.com>
2024-04-11 14:17:02 -0400

    “Changes”

--- hexagonal.py
+++ hexagonal.py
@@ -19,7 +19,7 @@ def parse():
 	parser.add_argument('-es', '--esmodulus', type = float, default = 0.01, help = 'Elastic Modulus for structural material')
 	parser.add_argument('-er', '--ermodulus', type = float, default = 1.0, help = 'Elastic Modulus for responsive material')
 	parser.add_argument('-p', '--power_p', type = float, default = 2.0, help = 'Power for elasticity interpolation')
-	parser.add_argument('-q', '--power_q', type = float, default = 2.0, help = 'Power for multiple-well function')
+	parser.add_argument('-q', '--power_q', type = float, default = 1.0, help = 'Power for multiple-well function')
 	parser.add_argument('-s', '--steamy', type = float, default = 1.0, help = 'Initial stimulus')
 	options = parser.parse_args()
 	return options
