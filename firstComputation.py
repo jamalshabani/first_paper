6f01c09f1c05124e750af00ba70d0517b21b0be8
jamalshabani <jjuma1992@gmail.com>
2024-05-31 19:09:33 -0400

    Initial stimulus is set to 0

--- firstComputation.py
+++ firstComputation.py
@@ -21,7 +21,7 @@ def parse():
 	parser.add_argument('-es', '--esmodulus', type = float, default = 0.01, help = 'Elastic Modulus for structural material')
 	parser.add_argument('-er', '--ermodulus', type = float, default = 1.0, help = 'Elastic Modulus for responsive material')
 	parser.add_argument('-p', '--power_p', type = float, default = 2.0, help = 'Power for elasticity interpolation + triple well potential')
-	parser.add_argument('-s', '--steamy', type = float, default = 1.0, help = 'Initial stimulus')
+	parser.add_argument('-s', '--steamy', type = float, default = 0.0, help = 'Initial stimulus')
 	options = parser.parse_args()
 	return options
 
