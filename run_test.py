ff966cbdf9ec385454e35bb1af66160f68bce2d8
jamalshabani <jjuma1992@gmail.com>
2024-06-13 16:13:07 -0400

    Script + convergence detials for ratio 10

--- run_test.py
+++ run_test.py
@@ -2,7 +2,7 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3", "rm -rf test4", "rm -rf test5",
-                "python3 alternateFirstComputation.py -tao_monitor -tao_max_it 10000 -tao_max_funcs 10000 -tao_view -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_ls_type unit -er 1.0e0 -es 1.0e-2 -lr 5.0 -ls -0.050 -k 8.0e-3 -e 2.0e-3 -vr 0.3 -vs 0.3"]
+                "python3 alternateFirstComputation.py -tao_monitor -tao_max_it 10000 -tao_max_funcs 10000 -tao_view -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_ls_type unit -er 1.0e0 -es 1.0e-1 -lr 4.0 -ls 0.237 -k 8.0e-3 -e 2.0e-3 -vr 0.3 -vs 0.3"]
 
 
 
